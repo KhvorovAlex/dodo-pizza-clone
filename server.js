b07e73a7c83f06e031e42a5fb438b40f3b7359fe
Alexandr Khvorov <Khvorov.aa@yandex.ru>
2020-09-14 17:37:49 +0700

    edit server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import jsonServer from 'json-server'
+const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('./public/db.json')
 const middlewares = jsonServer.defaults({
