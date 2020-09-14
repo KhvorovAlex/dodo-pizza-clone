1bb8b9051d70a5abde304f89c317f306148abc8b
Alexandr Khvorov <Khvorov.aa@yandex.ru>
2020-09-14 16:38:01 +0700

    Updates

---
+++ precache-manifest.568a2d1f68a36fe89c755c9dd66a1943.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd53e019a6ecf0bf038cc23a9d0a09e1",
+    "url": "/dodo-pizza-clone/index.html"
+  },
+  {
+    "revision": "4f3a874eee68df09f019",
+    "url": "/dodo-pizza-clone/static/css/main.66ee3d2b.chunk.css"
+  },
+  {
+    "revision": "ecbedd139dddb74df68f",
+    "url": "/dodo-pizza-clone/static/js/2.a65d26f2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dodo-pizza-clone/static/js/2.a65d26f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f3a874eee68df09f019",
+    "url": "/dodo-pizza-clone/static/js/main.a0fd5a5b.chunk.js"
+  },
+  {
+    "revision": "2ab78ad929c650175bac",
+    "url": "/dodo-pizza-clone/static/js/runtime-main.427a9cd5.js"
+  },
+  {
+    "revision": "4325ed848b66ed370420ea7fea09bc80",
+    "url": "/dodo-pizza-clone/static/media/ProximaNova-Black.4325ed84.eot"
+  },
+  {
+    "revision": "a0606c5fef28b9e57f501db027063141",
+    "url": "/dodo-pizza-clone/static/media/ProximaNova-Black.a0606c5f.ttf"
+  },
+  {
+    "revision": "b978d1b46a86df7a012cf8442936a1cb",
+    "url": "/dodo-pizza-clone/static/media/ProximaNova-Black.b978d1b4.woff"
+  },
+  {
+    "revision": "10141a9d737da9c84e6e14ec5a562c6c",
+    "url": "/dodo-pizza-clone/static/media/ProximaNova-Bold.10141a9d.eot"
+  },
+  {
+    "revision": "926a08fb27e3303c7452b0bdd2d5e5ab",
+    "url": "/dodo-pizza-clone/static/media/ProximaNova-Bold.926a08fb.ttf"
+  },
+  {
+    "revision": "e2cf3dc2f079bf3d5185a02552f153c4",
+    "url": "/dodo-pizza-clone/static/media/ProximaNova-Bold.e2cf3dc2.woff"
+  },
+  {
+    "revision": "169bc0f1eb2d8d0d8678014d368b0226",
+    "url": "/dodo-pizza-clone/static/media/ProximaNova-Extrabld.169bc0f1.eot"
+  },
+  {
+    "revision": "51563919fd1af7376fac14b1194a8e4e",
+    "url": "/dodo-pizza-clone/static/media/ProximaNova-Extrabld.51563919.ttf"
+  },
+  {
+    "revision": "ccf59c77109aecc8deeeede82e3bac87",
+    "url": "/dodo-pizza-clone/static/media/ProximaNova-Extrabld.ccf59c77.woff"
+  },
+  {
+    "revision": "2d2ae2556b24a45ff8d5ed86b07b5783",
+    "url": "/dodo-pizza-clone/static/media/ProximaNova-Regular.2d2ae255.woff"
+  },
+  {
+    "revision": "7ce6760d17685c466ba04d1b2c63c38b",
+    "url": "/dodo-pizza-clone/static/media/ProximaNova-Regular.7ce6760d.ttf"
+  },
+  {
+    "revision": "b7c512788e3c77b0196f0bace8a88418",
+    "url": "/dodo-pizza-clone/static/media/ProximaNova-Regular.b7c51278.eot"
+  },
+  {
+    "revision": "ceb2faf089a96bede51ccde3693d012b",
+    "url": "/dodo-pizza-clone/static/media/empty-cart.ceb2faf0.png"
+  },
+  {
+    "revision": "f3327011e80a4caf08837bd844e21996",
+    "url": "/dodo-pizza-clone/static/media/pizza-logo.f3327011.svg"
+  },
+  {
+    "revision": "6a5172e059e326db3935412820afe364",
+    "url": "/dodo-pizza-clone/static/media/pizza.6a5172e0.png"
+  }
+]);
